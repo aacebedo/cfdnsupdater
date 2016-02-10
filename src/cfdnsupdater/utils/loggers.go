212fca2dc32d8d13aa5e89f0f770d3963495e424
Alexandre ACEBEDO <alexandre@acebedo.fr>
2016-02-10 22:13:05 +0100

    Added support of file logging mode

--- src/cfdnsupdater/utils/loggers.go
+++ src/cfdnsupdater/utils/loggers.go
@@ -3,32 +3,50 @@ package utils
 import (
 	"github.com/op/go-logging"
 	"os"
+	"path/filepath"
+)
+
+type LoggingMode int
+
+const (
+	Consolelog  LoggingMode = 0 << iota
+	Filelog     LoggingMode = 1
+	Syslog      LoggingMode = 2
 )
 
 var logger = logging.MustGetLogger("cfdnsupdater.utils")
 
-func InitLoggers(verbose bool, quiet bool, syslog bool) (err error){
+func InitLoggers(verbose bool, quiet bool, loggingMode LoggingMode) (err error) {
 	var format logging.Formatter
-	
-  var backend logging.Backend
-  if syslog {
-    backend, err = logging.NewSyslogBackend("cfdnsupdater")
-    if err != nil {
-      return
-    }
-    format = logging.MustStringFormatter(`%{message}`)
-    
-  } else {
-    var outstream *os.File
-  	if quiet {
-  		outstream = os.NewFile(uintptr(3), "/dev/null")
-  	} else {
-  		outstream = os.Stderr
-  	}
-    backend = logging.NewLogBackend(outstream, "", 0)
-    format =  logging.MustStringFormatter(`%{color}%{time:15:04:05.000} | %{level:.10s} ▶%{color:reset} %{message}`)
-  }
-  
+
+	var backend logging.Backend
+
+	switch loggingMode {
+	case Syslog:
+		backend, err = logging.NewSyslogBackend("cfdnsupdater")
+		if err != nil {
+			return
+		}
+		format = logging.MustStringFormatter(`%{message}`)
+	case Consolelog:
+		var outstream *os.File
+		if quiet {
+			outstream = os.NewFile(uintptr(3), filepath.Join("/","dev","null"))
+		} else {
+			outstream = os.Stderr
+		}
+		backend = logging.NewLogBackend(outstream, "", 0)
+		format = logging.MustStringFormatter(`%{color}%{time:15:04:05.000} | %{level:.10s} ▶%{color:reset} %{message}`)
+	case Filelog:
+	  var file *os.File
+	  file, err = os.OpenFile(filepath.Join("/","tmp","cfdnsupdater"), os.O_RDWR|os.O_CREATE, 0600)
+	  if err != nil {
+	    return
+	  }
+	  backend = logging.NewLogBackend(file, "", 0)
+		format = logging.MustStringFormatter(`%{color}%{time:15:04:05.000} | %{level:.10s} ▶%{color:reset} %{message}`)
+	}
+
 	formatter := logging.NewBackendFormatter(backend, format)
 	leveledBackend := logging.AddModuleLevel(formatter)
 
@@ -39,5 +57,5 @@ func InitLoggers(verbose bool, quiet bool, syslog bool) (err error){
 	}
 
 	logging.SetBackend(leveledBackend)
-  return
+	return
 }
